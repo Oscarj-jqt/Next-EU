9f707835d67d44d688c9ebdc53ac50d8b204681e
Bagharia <hu.alexis@hotmail.com>
2025-03-20 11:00:42 +0100

    ajout des commentaire et like

--- frontend/src/app/page/Home.tsx
+++ frontend/src/app/page/Home.tsx
@@ -1,11 +1,9 @@
-"use client"; 
+"use client";
 
 import React from "react";
 import EuropeMap from "../components/EuropeMap";
 import { useRouter } from "next/navigation";
-import Header from "../components/header"; 
-import Category from "../components/Category";
-
+import Header from "../components/header";
 
 const Home: React.FC = () => {
   const router = useRouter();
@@ -13,21 +11,16 @@ const Home: React.FC = () => {
     <div className="flex flex-col items-center justify-center w-full h-screen bg-black">
     
       <Header />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
       
       <div className="w-[393px] h-[750px] overflow-hidden">
         <EuropeMap />
-        <div className="absolute bottom-5 w-[393px] flex justify-center z-3">
-          <Category />
-        </div>
       </div>
-      <button 
-        onClick={() => router.push("../connecteMessage")}>
-          <div className="text-3xl">
-            <i className="fa-solid fa-comments"></i>
-          </div>
-      
+
+      <button
+        onClick={() => router.push("../connecteMessage")}
+        className="w-[393px] p-3 bg-blue-600 text-white text-lg font-bold cursor-pointer mb-5"
+      >
+        Accéder au Chat
       </button>
     </div>
   );
